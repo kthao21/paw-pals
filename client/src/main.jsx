d38639f52f4a93aadb0da2a86e3d308d1996159c
Gabriel Rinaldi <gaberinaldi.bio@gmail.com>
2023-11-22 20:52:56 -0700

    fixed-pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
-// import Donation from './pages/Donation';
+import Donation from './pages/Donation';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-// import Contact from './pages/Contact';
-// import SavedSearches from './pages/SavedSearches';
+import Contact from './pages/Contact';
+import SavedSearches from './pages/SavedSearches';
 
 const router = createBrowserRouter([
   {
@@ -26,18 +26,18 @@ const router = createBrowserRouter([
         path: '/signup',
         element: <Signup />
       }, 
-      // {
-      //   path: '/contact',
-      //   element: <Contact />
-      // }, 
-      // {
-      //   path: '/donation',
-      //   element: <Donation />
-      // },  
-      // {
-      //   path: '/savedSearches',
-      //   element: <SavedSearches />
-      // }
+      {
+        path: '/contact',
+        element: <Contact />
+      }, 
+      {
+        path: '/donation',
+        element: <Donation />
+      },  
+      {
+        path: '/savedSearches',
+        element: <SavedSearches />
+      }
     ]
   }
 ]);
